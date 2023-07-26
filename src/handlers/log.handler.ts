98d099acff813a697edb66ae415b637e8325b679
Szedann <kapslok1211@gmail.com>
2023-07-26 18:09:04 +0200

    remove unused and add used, also change used (pain)

--- src/handlers/log.handler.ts
+++ src/handlers/log.handler.ts
@@ -2,116 +2,116 @@ import { EmbedBuilder, Events } from 'discord.js';
 
 // log providers
 import logProviders from '../logProviders/_logProviders';
-import logAnalyzers from '../logAnalyzers/_logAnalyzers';
+import logAnalyzers from '../logIssueAnalyzers/_logIssueAnalyzers';
 
 import { Handler } from '..';
 import { analyzers } from '../logs/Analyzer';
-
-export type LogAnalyzer = (
-  url: string
-) => Promise<null | { name: string; value: string }>;
 export interface LogProvider {
-  hostnames: string[];
-  parse: (url: string) => Promise<void | string>;
+    hostnames: string[];
+    parse: (url: string) => Promise<void | string>;
 }
+
+export type Analyzer = (
+    log: string
+) => Promise<{ name: string; value: string } | null>;
 
 const hostnameMap = new Map<string, (text: string) => Promise<void | string>>();
 
 for (const provider of logProviders) {
-  provider.hostnames.forEach((hostname) =>
-    hostnameMap.set(hostname, provider.parse)
-  );
+    provider.hostnames.forEach((hostname) =>
+        hostnameMap.set(hostname, provider.parse)
+    );
 }
 
 async function parseWebLog(text: string): Promise<string | void> {
-  const reg = text.match(
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
-  );
-  if (!reg) return;
-  const url = reg[0];
-  const hostname = url.split('/')[2];
-  if (!hostnameMap.has(hostname)) return;
-  return hostnameMap.get(hostname)!(url);
+    const reg = text.match(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+    );
+    if (!reg) return;
+    const url = reg[0];
+    const hostname = url.split('/')[2];
+    if (!hostnameMap.has(hostname)) return;
+    return hostnameMap.get(hostname)!(url);
 }
 
 async function findIssues(log: string) {
-  const issues: { name: string; value: string }[] = [];
+    const issues: { name: string; value: string }[] = [];
 
-  for (const analyzer of logAnalyzers) {
-    const issue = await analyzer(log);
-    if (issue) issues.push(issue);
-  }
+    for (const analyzer of logAnalyzers) {
+        const issue = await analyzer(log);
+        if (issue) issues.push(issue);
+    }
 
-  return issues;
+    return issues;
 }
 
 export const logHandler: Handler = (client) => {
-  client.on(Events.MessageCreate, async (message) => {
-    try {
-      if (message.channel.partial) await message.channel.fetch();
-      if (message.author.partial) await message.author.fetch();
+    client.on(Events.MessageCreate, async (message) => {
+        try {
+            if (message.channel.partial) await message.channel.fetch();
+            if (message.author.partial) await message.author.fetch();
 
-      const attachment = message.attachments.find(
-        (attachment) => attachment.contentType == 'text/plain; charset=utf-8'
-      );
+            const attachment = message.attachments.find(
+                (attachment) =>
+                    attachment.contentType == 'text/plain; charset=utf-8'
+            );
 
-      if (!message.content && !attachment) return;
-      if (!message.channel.isTextBased()) return;
+            if (!message.content && !attachment) return;
+            if (!message.channel.isTextBased()) return;
 
-      if (message.author === client.user) return;
+            if (message.author === client.user) return;
 
-      const log = attachment
-        ? await (await fetch(attachment.url)).text()
-        : await parseWebLog(message.content);
+            const log = attachment
+                ? await (await fetch(attachment.url)).text()
+                : await parseWebLog(message.content);
 
-      if (!log) return;
+            if (!log) return;
 
-      const regexPasses = [
-        /---- Minecraft Crash Report ----/, // Forge Crash report
-        /\n\\|[\\s\\d]+\\| Minecraft\\s+\\| minecraft\\s+\\| (\\S+).+\n/, // Quilt mod table
-        /: Loading Minecraft (\\S+)/, // Fabric, Quilt
-        /--fml.mcVersion, ([^\\s,]+)/, // Forge
-        /--version, ([^,]+),/, // ATLauncher
-        / --version (\\S+) /, // MMC, Prism, PolyMC
-      ];
+            const regexPasses = [
+                /---- Minecraft Crash Report ----/, // Forge Crash report
+                /\n\\|[\\s\\d]+\\| Minecraft\\s+\\| minecraft\\s+\\| (\\S+).+\n/, // Quilt mod table
+                /: Loading Minecraft (\\S+)/, // Fabric, Quilt
+                /--fml.mcVersion, ([^\\s,]+)/, // Forge
+                /--version, ([^,]+),/, // ATLauncher
+                / --version (\\S+) /, // MMC, Prism, PolyMC
+            ];
 
-      if (!regexPasses.find((reg) => log.match(reg))) return;
+            if (!regexPasses.find((reg) => log.match(reg))) return;
 
-      const logInfo: { name: string, value: string }[] = []
+            const logInfo: { name: string; value: string }[] = [];
 
-      for (const analyzer of analyzers) {
-        const info = await analyzer(log)
-        if (info) logInfo.push(info)
-      }
+            for (const analyzer of analyzers) {
+                const info = await analyzer(log);
+                if (info) logInfo.push(info);
+            }
 
-      const logInfoEmbed = new EmbedBuilder()
-        .setTitle("Log File")
-        .setDescription("__Environment info__")
-        .setColor("Green")
-        .setFields(...logInfo)
+            const logInfoEmbed = new EmbedBuilder()
+                .setTitle('Log File')
+                .setDescription('__Environment info__')
+                .setColor('Green')
+                .setFields(...logInfo);
 
-      const issues = await findIssues(log);
+            const issues = await findIssues(log);
 
-      if (!issues.length) {
-        message.reply({ embeds: [logInfoEmbed] })
-        return
-      }
+            if (!issues.length) {
+                message.reply({ embeds: [logInfoEmbed] });
+                return;
+            }
 
-      const issuesEmbed = new EmbedBuilder()
-        .setTitle('Log analysis')
-        .setDescription(
-          `${issues.length} issue${issues.length == 1 ? '' : 's'
-          } found automatically`
-        )
-        .setFields(...issues)
-        .setColor('Red');
+            const issuesEmbed = new EmbedBuilder()
+                .setTitle('Log analysis')
+                .setDescription(
+                    `${issues.length} issue${
+                        issues.length == 1 ? '' : 's'
+                    } found automatically`
+                )
+                .setFields(...issues)
+                .setColor('Red');
 
-
-      message.reply({ embeds: [logInfoEmbed, issuesEmbed] });
-      return;
-
-    } catch (error) {
-      console.error('Unhandled exception on MessageCreate', error);
-    }
-  });
+            message.reply({ embeds: [logInfoEmbed, issuesEmbed] });
+            return;
+        } catch (error) {
+            console.error('Unhandled exception on MessageCreate', error);
+        }
+    });
 };
--- src/logs/Analyzer.ts
+++ src/logs/Analyzer.ts
@@ -1,108 +1,107 @@
-import { EmbedBuilder } from 'discord.js';
+import { Analyzer } from '../handlers/log.handler';
 
-export type Analyzer = (log: string) => Promise<{ name: string, value: string } | null>;
+export const getMinecraftVersion = (log: string) => {
+    const minecraftVerionRegexes = [
+        /\n\|[\s\d]+\| Minecraft\s+\| minecraft\s+\| (\S+).+\n/,
+        /: Loading Minecraft (\S+)/,
+        /--fml.mcVersion, ([^\s,]+)/,
+        /--version, ([^,]+),/,
+        / --version (\\S+) /,
+        /Minecraft Version: (.+)/,
+    ];
 
-export const minecraftVersionAnalyser: Analyzer = async (log) => {
-  const minecraftVerionRegexes = [
-    /\n\|[\s\d]+\| Minecraft\s+\| minecraft\s+\| (\S+).+\n/,
-    /: Loading Minecraft (\S+)/,
-    /--fml.mcVersion, ([^\s,]+)/,
-    /--version, ([^,]+),/,
-    / --version (\\S+) /,
-    /Minecraft Version: (.+)/
-  ]
-
-  for (const regex of minecraftVerionRegexes) {
-    const regRes = log.match(regex)
-    if (!regRes) continue
-    const version = regRes[1].toString()
-    return {
-      name: 'Minecraft Version',
-      value: "`" + version + "`"
+    for (const regex of minecraftVerionRegexes) {
+        const regRes = log.match(regex);
+        if (!regRes) continue;
+        return regRes[1].toString();
     }
-  }
-  return null;
+};
+
+const minecraftVersionAnalyser: Analyzer = async (log) => {
+    const version = getMinecraftVersion(log);
+    if (!version) return null;
+    return {
+        name: 'Minecraft Version',
+        value: '`' + version + '`',
+    };
+};
+
+export const getJavaVersion = (log: string) => {
+    const javaVersionRegexes = [/Java Version: (.+)/, /Java is version (.+?),/];
+    for (const regex of javaVersionRegexes) {
+        const regRes = log.match(regex);
+        if (!regRes) continue;
+        const version = regRes[1].toString();
+        return version;
+    }
 };
 
 export const javaVersionAnalyzer: Analyzer = async (log) => {
-  const javaVersionRegexes = [
-    /Java Version: (.+)/,
-    /Java is version (.+?),/,
-  ]
-  for (const regex of javaVersionRegexes) {
-    const regRes = log.match(regex)
-    if (!regRes) continue
-    const version = regRes[1].toString()
+    const version = getJavaVersion(log);
+    if (!version) return null;
     return {
-      name: 'Java Version',
-      value: "`" + version + "`"
-    }
-  }
-  return null
-}
-
-
-export const javaArgumentsAnalyzer: Analyzer = async (text) => {
-  const javaArgumentsRegex = /Java Arguments:\s+(\[[^\]]+\])/;
-  const javaArgumentsMatch = text.match(javaArgumentsRegex);
-
-  if (javaArgumentsMatch) {
-    const javaargs = javaArgumentsMatch[1].toString();
-    return {
-      name: 'Java Arguments',
-      value: `\`\`\`${javaargs}\`\`\``
+        name: 'Java Version',
+        value: '`' + version + '`',
     };
-  }
-  return null;
 };
 
-export type Loader = "Quilt" | "Fabric" | "Forge"
+export const javaArgumentsAnalyzer: Analyzer = async (text) => {
+    const javaArgumentsRegex = /Java Arguments:\s+(\[[^\]]+\])/;
+    const javaArgumentsMatch = text.match(javaArgumentsRegex);
 
-export function getLoader(log: string): { loader: Loader, version: string } | undefined {
-  const loaderRegexes = new Map<Loader, RegExp[]>
-  loaderRegexes.set("Quilt",
-    [
-      /\n\|[\s\d]+\| Quilt Loader\s+\| quilt_loader\s+\| (\S+).+\n/,
-      /: Loading .+ with Quilt Loader (\S+)/
-    ])
-  loaderRegexes.set("Fabric",
-    [
-      /: Loading .+ with Fabric Loader (\S+)/
-    ])
-  loaderRegexes.set("Forge", [
-    /--fml.forgeVersion, ([^\s,]+)/,
-    /MinecraftForge v([^\\s,]+) Initialized/
-  ])
-
-
-  for (const loader of loaderRegexes.keys()) {
-    const regexes = loaderRegexes.get(loader)
-    if (!regexes) continue
-    for (const regex of regexes) {
-      const regRes = log.match(regex)
-      if (!regRes) continue
-      const version = regRes[1].toString()
-      return {
-        loader: loader,
-        version: version
-      }
+    if (javaArgumentsMatch) {
+        const javaargs = javaArgumentsMatch[1].toString();
+        return {
+            name: 'Java Arguments',
+            value: `\`\`\`${javaargs}\`\`\``,
+        };
     }
-  }
+    return null;
+};
+
+export type Loader = 'Quilt' | 'Fabric' | 'Forge';
+
+export function getLoader(
+    log: string
+): { loader: Loader; version: string } | undefined {
+    const loaderRegexes = new Map<Loader, RegExp[]>();
+    loaderRegexes.set('Quilt', [
+        /\n\|[\s\d]+\| Quilt Loader\s+\| quilt_loader\s+\| (\S+).+\n/,
+        /: Loading .+ with Quilt Loader (\S+)/,
+    ]);
+    loaderRegexes.set('Fabric', [/: Loading .+ with Fabric Loader (\S+)/]);
+    loaderRegexes.set('Forge', [
+        /--fml.forgeVersion, ([^\s,]+)/,
+        /MinecraftForge v([^\\s,]+) Initialized/,
+    ]);
+
+    for (const loader of loaderRegexes.keys()) {
+        const regexes = loaderRegexes.get(loader);
+        if (!regexes) continue;
+        for (const regex of regexes) {
+            const regRes = log.match(regex);
+            if (!regRes) continue;
+            const version = regRes[1].toString();
+            return {
+                loader: loader,
+                version: version,
+            };
+        }
+    }
 }
 
 const loaderAnalyser: Analyzer = async (log) => {
-  const loaderData = getLoader(log)
-  if (!loaderData) return null
-  const { loader, version } = loaderData
-  return {
-    name: "Loader",
-    value: `\`${loader} (${version})\``
-  }
+    const loaderData = getLoader(log);
+    if (!loaderData) return null;
+    const { loader, version } = loaderData;
+    return {
+        name: 'Loader',
+        value: `\`${loader} (${version})\``,
+    };
 };
 
-
 export const analyzers: Analyzer[] = [
-  minecraftVersionAnalyser,
-  javaVersionAnalyzer,
-  loaderAnalyser
-]
+    minecraftVersionAnalyser,
+    javaVersionAnalyzer,
+    loaderAnalyser,
+];
