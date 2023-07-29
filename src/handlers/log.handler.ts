edfda4b6684d5220326611902760b3c2f90a1d92
Szedann <kapslok1211@gmail.com>
2023-07-29 12:30:27 +0200

    run prettier

--- src/handlers/log.handler.ts
+++ src/handlers/log.handler.ts
@@ -105,7 +105,9 @@ export const logHandler: Handler = (client) => {
             const issuesEmbed = new EmbedBuilder()
                 .setTitle('Log analysis')
                 .setDescription(
-                    `${issues.length} issue${issues.length == 1 ? '' : 's'} found automatically`
+                    `${issues.length} issue${
+                        issues.length == 1 ? '' : 's'
+                    } found automatically`
                 )
                 .setFields(...issues)
                 .setColor('Red');
