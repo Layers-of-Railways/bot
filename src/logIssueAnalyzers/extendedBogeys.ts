03fb1a8f3e0527e70640d7ee5557419c08534a35
Szedann <65607498+Szedann@users.noreply.github.com>
2023-09-22 17:18:20 +0200

    fix the extended bogeys issue analyzer

--- src/logIssueAnalyzers/extendedBogeys.ts
+++ src/logIssueAnalyzers/extendedBogeys.ts
@@ -3,12 +3,14 @@ import { Analyzer } from '../handlers/log.handler';
 export const extendedBogeysAnalyzer: Analyzer = async (log) => {
     if (!log.mods) return null;
 
-    const matchesExtendedBogeys = log.mods
-        ? log.mods.has('extendedbogeys')
-        : log.content.match(
-              /NoSuchMethodError: 'com\.jozufozu\.flywheel\.util\.transform\.Transform\[\]/
-          );
-    if (matchesExtendedBogeys) {
+    const matchesCreate = log.mods.get('create')?.match(/0.5.1.[c|d|e]/);
+
+    const matchesExtendedBogeys =
+        log.mods.has('extendedbogeys') &&
+        log.content.match(
+            /NoSuchMethodError: 'com\.jozufozu\.flywheel\.util\.transform\.Transform\[\]/
+        );
+    if (matchesCreate && matchesExtendedBogeys) {
         return {
             name: 'Extended Bogeys',
             value: "Extended Bogeys has not yet updated to work with `0.5.1c/d`. Please downgrade to `0.5.1b`. And downgrade Steam 'n' Rails.",
