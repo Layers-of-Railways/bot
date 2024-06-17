77cc362532fa5aeffe9fc46eb82303eeb9368d7b
IThundxr <IThundxr@users.noreply.github.com>
2024-06-17 14:40:21 +0000

    Apply automatic changes

--- src/logIssueAnalyzers/_logIssueAnalyzers.ts
+++ src/logIssueAnalyzers/_logIssueAnalyzers.ts
@@ -2,6 +2,9 @@ import { Analyzer } from '../handlers/log.handler';
 import { createVersionAnalyzer } from './createVersion';
 import { farmersDelightNPE } from './farmersDelightNPE';
 
-export const logAnalyzers: Analyzer[] = [createVersionAnalyzer, farmersDelightNPE];
+export const logAnalyzers: Analyzer[] = [
+    createVersionAnalyzer,
+    farmersDelightNPE,
+];
 
 export default logAnalyzers;
--- src/logIssueAnalyzers/farmersDelightNPE.ts
+++ src/logIssueAnalyzers/farmersDelightNPE.ts
@@ -3,11 +3,13 @@ import { Analyzer } from '../handlers/log.handler';
 export const farmersDelightNPE: Analyzer = async (log) => {
     if (!log.mods) return null;
 
-    const regex = log.content.match(/NullPointerException.*class_2960.*farmersdelight\$migrateGet/gs)
+    const regex = log.content.match(
+        /NullPointerException.*class_2960.*farmersdelight\$migrateGet/gs
+    );
     if (regex) {
         return {
             name: 'Farmers Delight Missing Null check',
-            value: "Farmers Delight is missing a null check, update farmers delight",
+            value: 'Farmers Delight is missing a null check, update farmers delight',
         };
     }
 
